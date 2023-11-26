896d9541769e6475731268a05bb429c3f0f40447
calculatorai <chatgptprompt@163.com>
2023-11-26 22:52:20 +0800

    #

--- code/next.config.js
+++ code/next.config.js
@@ -1,3 +1,5 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
@@ -5,12 +7,31 @@ const withNextra = require('nextra')({
 
 const nextConfig = {
   output: 'export',
+  // basePath: '/code', // Set your repository name
+  // assetPrefix: '/code/',
   images: {
-    unoptimized:true,
+    unoptimized: true,
     // loader: 'custom',
     // loaderFile: './my-loader.ts',
   },
-  distDir: 'dist'
+  distDir: 'dist',
+  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  //   // 在构建完成后，复制自定义资源到 _next/static 目录
+  //   // if (!dev) {
+  //     console.log('Executing custom webpack configuration');
+  //     config.plugins.push(
+  //       new CopyWebpackPlugin({
+  //         patterns: [
+  //           {
+  //             from: 'dist/_next',
+  //             to: 'dist/prompt',
+  //           },
+  //         ],
+  //       })
+  //     );
+  //   // }
+  //   return config;
+  // },
 }
 
 module.exports = withNextra(nextConfig)
